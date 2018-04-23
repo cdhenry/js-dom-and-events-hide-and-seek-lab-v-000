7909400400a3e8010aaf15de35d284e1e17bcbbc
Chris Henry <cdhenry@gmail.com>
2018-04-23 22:11:22 +0000

    Done.

--- index.js
+++ index.js
@@ -9,12 +9,22 @@ function nestedTarget(){
 function increaseRankBy(n){
   const lis = document.querySelectorAll('.ranked-list')
 
-  for (let i = 0; i < lis.length; i++) {
-    lis[i].innerHTML = (i + n).toString()
+  for (let i = 0, l = lis.length; i < l; i++) {
+    let children = lis[i].children
+
+    for (let j = 0, k = children.length; j < k; j++)
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
   }
 }
 
 function deepestChild(){
-  const node = document.getElementById('grand-node').querySelectorAll('div')
-  return node[node.length-1];
+  const node = document.getElementById('grand-node');
+  let nextNode = node.children[0];
+
+  while (nextNode) {
+    node = nextNode;
+    nextNode = node.chilren[0];
+  }
+
+  return node;
 }
